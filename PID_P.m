688d64c5f1081e5ce51047dfb13c16eff38ef3b9
Jon <1575456904@qq.com>
2016-07-17 14:42:36 +0800

    PID比例作用分析

--- PID_P.m
+++ PID_P.m
@@ -5,11 +5,11 @@
 G0=tf(1,[1,3,3,1]);
 P=[0.1 0.3 0.5 1 2 3];
 figure,hold on
-for i=1:length(P)
-    G=feedback(P(i)*G0,1);
-    step(G)
+for i=1:length(P);
+    G(i)=feedback(P(i)*G0,1);
+    step(G(i))
 end
 axis([0,12,0,1.3]),grid on,hold off
-figure
-rlocus(G0),axis([-2,0.2,-2,2])
-k=rlocfind(G0)
+% figure
+% rlocus(G0),axis([-2,0.2,-2,2])
+% k=rlocfind(G0)
